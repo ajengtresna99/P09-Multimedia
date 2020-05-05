93db17ecd1cc1a45e92be6c253a7c43bc7b4d81b
ajengtresna99 <56184633+ajengtresna99@users.noreply.github.com>
2020-05-05 19:24:05 +0700

    Add files via upload

---
+++ PhbMediaPlayer/src/phbmediaplayer/PhbMediaPlayer.java
@@ -0,0 +1,40 @@
+
+package phbmediaplayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author WIN10
+ */
+public class PhbMediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("PHB Media Player");
+        scene.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getClickCount() == 2) {
+                stage.setFullScreen(true);
+            }
+        });
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
